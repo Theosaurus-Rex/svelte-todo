ef2dcb63ce512cea7c6a8aaa9babc8bfd040bf4d
Theosaurus-Rex <theosaurus13@gmail.com>
2024-12-21 14:58:53 +1100

    feat: Add task filters

--- src/components/tasks-list.svelte
+++ src/components/tasks-list.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import type { Task } from '../types';
 	let {
 		tasks,
 		toggleDone,
 		removeTask
-	}: { tasks: Task[]; toggleDone: (task: Task) => void; removeTask: (index: number) => void } =
+	}: { tasks: Task[]; toggleDone: (task: Task) => void; removeTask: (id: string) => void } =
 		$props();
 </script>
 
 <section>
-	{#each tasks as task, index}
-		<article>
+	{#each tasks as task}
+		<article transition:fade>
 			<label>
 				<input type="checkbox" checked={task.done} onchange={() => toggleDone(task)} />
 				{task.title}
 			</label>
-			<button onclick={() => removeTask(index)}>Remove</button>
+			<button onclick={() => removeTask(task.id)}>Remove</button>
 		</article>
 	{/each}
 </section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
 	import TasksForm from '../components/tasks-form.svelte';
 	import TasksList from '../components/tasks-list.svelte';
-	import type { Task } from '../types';
+	import type { Filter, Task } from '../types';
+
+	let currentFilter = $state<Filter>('all');
 	let tasks = $state<Task[]>([]);
 	let totalDone = $derived(tasks.reduce((total, task) => total + Number(task.done), 0));
 
+	let filteredTasks = $derived.by(() => {
+		switch (currentFilter) {
+			case 'all':
+				return tasks;
+			case 'done':
+				return tasks.filter((task) => task.done);
+			case 'todo':
+				return tasks.filter((task) => !task.done);
+		}
+		return tasks;
+	});
+
 	function addTask(newTask: string) {
-		// This now triggers a re-render in svelte 5
 		tasks.push({
 			id: crypto.randomUUID(),
 			title: newTask,
@@ -18,14 +31,34 @@
 		task.done = !task.done;
 	}
 
-	function removeTask(index: number) {
+	function removeTask(id: string) {
+		const index = tasks.findIndex((task) => task.id === id);
 		tasks.splice(index, 1);
 	}
 </script>
 
+{#snippet filterButton(filter: Filter)}
+	<button class="capitalize" onclick={() => (currentFilter = filter)}>
+		{filter}
+	</button>
+{/snippet}
+
 <main>
 	<h1>Tasks App</h1>
 	<TasksForm {addTask} />
-	<p>{totalDone} / {tasks.length} tasks completed</p>
-	<TasksList {tasks} {toggleDone} {removeTask} />
+	<p>
+		{#if tasks.length}
+			{totalDone} / {tasks.length} tasks completed
+		{:else}
+			Add a task to get started.
+		{/if}
+	</p>
+	{#if tasks.length}
+		<div>
+			{@render filterButton('all')}
+			{@render filterButton('done')}
+			{@render filterButton('todo')}
+		</div>
+	{/if}
+	<TasksList tasks={filteredTasks} {toggleDone} {removeTask} />
 </main>
